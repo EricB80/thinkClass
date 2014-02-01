ee2b95029545d2a1cc3821bcf0ab43bd40fb20aa
lazarus99 <eric.berkow@gmail.com>
2014-02-01 23:16:24 +0000

    add v3 finally works

--- tip_calc/tip_cal1.rb
+++ tip_calc/tip_cal1.rb
@@ -1,10 +1,10 @@
-meal = 25.36
-tax_rate = 5.6
+meal = 100
+tax_rate = 5
 tip_rate = 20
 
-tax_value = meal * (tax_rate / 100)
+tax_value = meal * tax_rate / 100
 after_tax = meal + tax_value
-tip_value = meal + (tip_rate / 100)
+tip_value = meal * tip_rate / 100
 total_cost = after_tax + tip_value
 
 puts "the meal cost: #{meal}"
--- tip_calc/tip_calc3.rb
+++ tip_calc/tip_calc3.rb
@@ -5,12 +5,20 @@ tax_rate = Float(gets)
 puts "What percentage of tip would you like to leave?"
 tip_rate = Float(gets)
 
-tax_value = ((meal * (tax_rate / 100))*100).round() / 100.0
-after_tax = ((meal + tax_value)*100).round() / 100.0
-tip_value = ((meal + (tip_rate / 100))*100).round() / 100.0
-total_cost = ((after_tax + tip_value)*100).round() / 100.0
+def calc_value(rate, amount)
+  return (amount * (rate / 100)).round(2)
+end
 
-puts "the meal cost: #{meal}"
-puts "the tax value is: #{tax_value}"
-puts "the tip is: #{tip_value}"
-puts "the total cost is: #{total_cost}"
+def force_currency(num)
+  return sprintf("%.02f", num)
+end
+
+tax_value = calc_value(meal, tax_rate)
+after_tax = tax_value + meal
+tip_value = calc_value(meal, tip_rate)
+total_cost = (after_tax + tip_value)
+
+puts "the meal cost: " + force_currency(meal)
+puts "the tax value is: " + force_currency(tax_value)
+puts "the tip is: " + force_currency(tip_value)
+puts "the total cost is: " + force_currency(total_cost)
